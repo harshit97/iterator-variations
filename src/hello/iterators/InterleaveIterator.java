2033817aff5616bb0876a7af5ded3765229f5c33
harshagarwal <harshagarwal@paypal.com>
2022-02-06 16:05:15 +0530

    Simple InterleaveIterator and basic test

---
+++ src/hello/iterators/InterleaveIterator.java
@@ -0,0 +1,44 @@
+package hello.iterators;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+public class InterleaveIterator<E> implements Iterator<E> {
+
+    private final List<Iterator<E>> iteratorList;
+    private Iterator<E> currentIterator;
+    private int iteratorIndex;
+
+    InterleaveIterator(List<Iterator<E>> iteratorList) {
+        this.iteratorList = iteratorList.stream().filter(Iterator::hasNext).collect(Collectors.toList());
+        iteratorIndex = 0;
+        currentIterator = this.iteratorList.get(iteratorIndex);
+    }
+
+    @Override
+    public boolean hasNext() {
+        return currentIterator.hasNext();
+    }
+
+    @Override
+    public E next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        E elementToReturn = currentIterator.next();
+        int currentIteratorIndex = iteratorIndex;
+        Iterator<E> nextIterator = iteratorList.get(incrementAndGetIteratorIndex());
+        while (!nextIterator.hasNext() && currentIteratorIndex != iteratorIndex) {
+            nextIterator = iteratorList.get(incrementAndGetIteratorIndex());
+        }
+        currentIterator = nextIterator;
+        return elementToReturn;
+    }
+
+    private int incrementAndGetIteratorIndex() {
+        iteratorIndex = (iteratorIndex + 1) % iteratorList.size();
+        return iteratorIndex;
+    }
+}
--- src/hello/iterators/Main.java
+++ src/hello/iterators/Main.java
@@ -1,8 +1,25 @@
 package hello.iterators;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    List<String> aList = Arrays.asList("a1", "a2", "a3", "a4", "a5");
+        List<String> bList = Arrays.asList("b1", "b2", "b3", "b4");
+        List<String> cList = Arrays.asList("c1", "c2", "c3");
+
+        List<Iterator<String>> iteratorList = new ArrayList<>();
+        iteratorList.add(aList.iterator());
+        iteratorList.add(bList.iterator());
+        iteratorList.add(cList.iterator());
+
+        InterleaveIterator<String> interleaveIterator = new InterleaveIterator(iteratorList);
+        while (interleaveIterator.hasNext()) {
+            System.out.print(interleaveIterator.next() + ", ");
+        }
     }
 }
